1236a3ed12faec65ce0205663c0ee785db0ef3fd
KingsSack <sanjaybrink@gmail.com>
2022-09-19 13:26:55 -0700

    v1.0.7

--- console.py
+++ console.py
@@ -1,10 +1,12 @@
 import savedata
+import game
 
 def console_commands():
-    command = input()
-    if command == "!speed":
-        new_speed1 = input("Enter new speed1: ")
-        savedata.speed1 = new_speed1
-        new_speed2 = input("Enter new speed2: ")
-        savedata.speed2 = new_speed2
+    if input() == "!spell thunder":
+        game.main_character.wizardThunder()
+    # if input() == "!speed":
+    #     new_speed1 = input("Enter new speed1: ")
+    #     savedata.speed1 = new_speed1
+    #     new_speed2 = input("Enter new speed2: ")
+    #     savedata.speed2 = new_speed2
         
