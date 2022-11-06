c111c384d677f5252c4d9488ac6e9402e1177ea8
KingsSack <sanjaybrink@gmail.com>
2022-11-06 09:51:12 -0800

    v0.1.29

--- collisions.py
+++ collisions.py
@@ -27,41 +27,41 @@ def stage_one():
     # print(game.stage1_sprites_list.sprites())
     # print(pygame.sprite.spritecollideany(game.main_character, game.stage1_sprites_list))
     
-    # if pygame.sprite.spritecollideany(game.main_character, game.stage1_sprites_list):
+    if pygame.sprite.spritecollideany(game.main_character, game.stage1_sprites_list):
     # if pygame.sprite.spritecollide(game.main_character, game.stage1_sprites_list, False, collided):
-    #     if direction == "left":
-    #         game.main_character.rect.x += 3
-    #     if direction == "right":
-    #         game.main_character.rect.x -= 3
-    #     if direction == "down":
-    #         game.main_character.rect.y += 3
-    #     if direction == "up":
-    #         game.main_character.rect.y -= 3
+        if direction == "left":
+            game.main_character.rect.x += 3
+        if direction == "right":
+            game.main_character.rect.x -= 3
+        if direction == "down":
+            game.main_character.rect.y += 3
+        if direction == "up":
+            game.main_character.rect.y -= 3
         
     
-    if -125 < game.main_character.rect.y < 70 and 290 > game.main_character.rect.x > 270:
-        game.main_character.rect.x += 3
-    if -125 < game.main_character.rect.y < 70 and 120 > game.main_character.rect.x > 100:
-        game.main_character.rect.x -= 3
-    if 65 < game.main_character.rect.y < 85 and 280 > game.main_character.rect.x > 120:
-        game.main_character.rect.y += 3
-    if -144 < game.main_character.rect.y < -124 and 280 > game.main_character.rect.x > 120:
-        game.main_character.rect.y -= 3
+    # if -125 < game.main_character.rect.y < 70 and 290 > game.main_character.rect.x > 270:
+    #     game.main_character.rect.x += 3
+    # if -125 < game.main_character.rect.y < 70 and 120 > game.main_character.rect.x > 100:
+    #     game.main_character.rect.x -= 3
+    # if 65 < game.main_character.rect.y < 85 and 280 > game.main_character.rect.x > 120:
+    #     game.main_character.rect.y += 3
+    # if -144 < game.main_character.rect.y < -124 and 280 > game.main_character.rect.x > 120:
+    #     game.main_character.rect.y -= 3
     
-    if -125 < game.main_character.rect.y < 70 and 385 > game.main_character.rect.x > 365:
-        game.main_character.rect.x -= 3
-    if -125 < game.main_character.rect.y < 70 and 550 > game.main_character.rect.x > 530:
-        game.main_character.rect.x += 3
-    if 65 < game.main_character.rect.y < 85 and 550 > game.main_character.rect.x > 370:
-        game.main_character.rect.y += 3
-    if -144 < game.main_character.rect.y < -124 and 550 > game.main_character.rect.x > 370:
-        game.main_character.rect.y -= 3
+    # if -125 < game.main_character.rect.y < 70 and 385 > game.main_character.rect.x > 365:
+    #     game.main_character.rect.x -= 3
+    # if -125 < game.main_character.rect.y < 70 and 550 > game.main_character.rect.x > 530:
+    #     game.main_character.rect.x += 3
+    # if 65 < game.main_character.rect.y < 85 and 550 > game.main_character.rect.x > 370:
+    #     game.main_character.rect.y += 3
+    # if -144 < game.main_character.rect.y < -124 and 550 > game.main_character.rect.x > 370:
+    #     game.main_character.rect.y -= 3
     
-    if -10 < game.main_character.rect.y < 70 and -6 > game.main_character.rect.x > -10:
-        game.main_character.rect.x -= 3
-    if -10 < game.main_character.rect.y < 70 and 48 > game.main_character.rect.x > 44:
-        game.main_character.rect.x += 3
-    if 70 < game.main_character.rect.y < 74 and 48 > game.main_character.rect.x > -10:
-        game.main_character.rect.y += 3
-    if -30 < game.main_character.rect.y < -26 and 48 > game.main_character.rect.x > -10:
-        game.main_character.rect.y -= 3
+    # if -10 < game.main_character.rect.y < 70 and -6 > game.main_character.rect.x > -10:
+    #     game.main_character.rect.x -= 3
+    # if -10 < game.main_character.rect.y < 70 and 48 > game.main_character.rect.x > 44:
+    #     game.main_character.rect.x += 3
+    # if 70 < game.main_character.rect.y < 74 and 48 > game.main_character.rect.x > -10:
+    #     game.main_character.rect.y += 3
+    # if -30 < game.main_character.rect.y < -26 and 48 > game.main_character.rect.x > -10:
+    #     game.main_character.rect.y -= 3
